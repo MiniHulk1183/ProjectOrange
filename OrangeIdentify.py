044d19c8faf2d839ac0dd48ed33e11c1e8e18007
Felipe Netto <83131932+MiniHulk1183@users.noreply.github.com>
2021-07-24 17:33:40 -0300

    Versão 2 da parte 1 do código para detecção de laranjas.

--- OrangeIdentify.py
+++ OrangeIdentify.py
@@ -1,16 +1,22 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('Imagens/Laranja_1.jpg')
+img = cv.imread('Imagens/Laranja_3.jpg')
 # cv.imshow('Laranjas', img)
 
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 # cv.imshow('HSV', hsv)
 
-h, s, v = cv.split(hsv)
+h,s,v = cv.split(hsv)
 
-cv.imshow('H', h)
-cv.imshow('S', s)
-cv.imshow('V', v)
+# cv.imshow('H', h)
+# cv.imshow('S', s)
+# cv.imshow('V', v)
+
+mask = cv.inRange(h, 1, 25)
+# cv.imshow('Mask', mask)
+
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow('Final', masked)
 
 cv.waitKey(0)
